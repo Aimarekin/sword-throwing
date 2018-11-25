f962debc0ff5fe11d06a84b4cd76763135b71710
Aimarekin <pol17@gmx.com>
2018-11-25 18:48:16 +0100

    Initial commit

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1543165957,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/character-sheet0.png",
+		"images/character-sheet1.png",
+		"images/ground-sheet0.png",
+		"images/sword-sheet0.png",
+		"images/box-sheet0.png",
+		"images/heart-sheet0.png",
+		"images/one_up-sheet0.png",
+		"media/1-up.ogg",
+		"media/damage.ogg",
+		"media/main.ogg",
+		"media/menu.ogg",
+		"media/game over.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"adapter.js"
+	]
+}
